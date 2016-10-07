e288f387eb842d9e33314b66bcc0876a7e67dfb4
Brandon Danis <brandon@spottedu.co>
2016-10-07 15:14:11 -0400

    Profile page UICollection view created and delegate were set

--- FriendStagram/ProfileViewController.swift
+++ FriendStagram/ProfileViewController.swift
@@ -9,16 +9,23 @@
 import Foundation
 import UIKit
 
-class ProfileViewController : UIViewController {
+class ProfileViewController : UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+    var images = ["http://4.bp.blogspot.com/-F_6SfcFHKRE/UIjJKWfbt8I/AAAAAAAAA6w/AK5H_oGl9io/s1600/nature182.jpg","http://rising.blackstar.com/wp-content/uploads/2012/08/95432c1c89bd11e1a9f71231382044a1_7-450x450.jpg","https://s-media-cache-ak0.pinimg.com/originals/58/50/77/585077f705e1e1e6385940fee0e6a4d7.jpg","http://4.bp.blogspot.com/-F_6SfcFHKRE/UIjJKWfbt8I/AAAAAAAAA6w/AK5H_oGl9io/s1600/nature182.jpg"];
     
     @IBOutlet var profileHeaderView : UIView!
     @IBOutlet var profilePicture : UIImageView!
     @IBOutlet var profileNameLabel : UILabel!
     
+    @IBOutlet var collectionView : UICollectionView!
+    
     
     override func viewDidLoad() {
         print("Profile View Loaded")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         
         profileHeaderView.backgroundColor = UIColor.white
         
@@ -35,6 +42,35 @@ class ProfileViewController : UIViewController {
     
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        
+        imageView.image = UIImage(named: "bg")
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        print("Getting cell size")
+        return CGSize(width: collectionView.frame.width/3, height: collectionView.frame.width/3)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
 }
