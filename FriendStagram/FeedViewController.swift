d1649e65139660ab5d4f09cf6bc6a97ec10c9d6a
Brandon Danis <brandon@spottedu.co>
2017-03-19 17:40:45 -0400

    Remove old array of posts

--- FriendStagram/FeedViewController.swift
+++ FriendStagram/FeedViewController.swift
@@ -11,84 +11,6 @@ import UIKit
 
 class FeedViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-//    var posts = [
-//        [
-//            "user": "Brandon",
-//            "imageURL": "http://4.bp.blogspot.com/-F_6SfcFHKRE/UIjJKWfbt8I/AAAAAAAAA6w/AK5H_oGl9io/s1600/nature182.jpg",
-//            "likes": "30"
-//        ],
-//        [
-//            "user": "Kyle",
-//            "imageURL": "http://rising.blackstar.com/wp-content/uploads/2012/08/95432c1c89bd11e1a9f71231382044a1_7-450x450.jpg",
-//            "likes": "142"
-//        ],
-//        [
-//            "user": "Rushil",
-//            "imageURL": "http://i.imgur.com/tLhJOrE.png",
-//            "likes": "74"
-//        ],
-//        [
-//            "user": "Minhthao",
-//            "imageURL": "http://4.bp.blogspot.com/-F_6SfcFHKRE/UIjJKWfbt8I/AAAAAAAAA6w/AK5H_oGl9io/s1600/nature182.jpg",
-//            "likes": "157"
-//        ],
-//        [
-//            "user": "Vivian",
-//            "imageURL": "http://blog.ink361.com/wp-content/uploads/2014/11/@hamiltonguevara-.jpg",
-//            "likes": "1263"
-//        ],
-//        [
-//            "user": "Minhthao",
-//            "imageURL": "https://s-media-cache-ak0.pinimg.com/564x/94/a9/db/94a9db37f653d29075f14611c1bd7359.jpg",
-//            "likes": "235"
-//        ],
-//        [
-//            "user": "Minhthao",
-//            "imageURL": "http://blogcdn.befunky.com/wp-content/uploads/2015/11/Screen-shot-2015-11-09-at-12.55.33-PM.png",
-//            "likes": "12"
-//        ],
-//        [
-//            "user": "Vivian",
-//            "imageURL": "http://hahaha.in/wp-content/uploads/2013/04/8e744a5aa38511e2b8e822000a1fbcc7_72.jpg",
-//            "likes": "74"
-//        ],
-//        [
-//            "user": "Minhthao",
-//            "imageURL": "http://s1.favim.com/610/150710/art-background-beautiful-dark-Favim.com-2933701.jpg",
-//            "likes": "73"
-//        ],
-//        [
-//            "user": "Brandon",
-//            "imageURL": "http://66.media.tumblr.com/tumblr_mdd8uyvpgQ1reash6o2_1280.jpg",
-//            "likes": "98"
-//        ],
-//        [
-//            "user": "Kyle",
-//            "imageURL": "http://stupiddope.com/wp-content/uploads/2014/07/cory-staudacher-instagram-03.jpg",
-//            "likes": "636"
-//        ],
-//        [
-//            "user": "Brandon",
-//            "imageURL": "http://static.boredpanda.com/blog/wp-content/uploads/2015/09/nature-photography-men-of-instagram-16__605.jpg",
-//            "likes": "478"
-//        ],
-//        [
-//            "user": "Vivian",
-//            "imageURL": "http://i.imgur.com/mpHgYA0.jpg",
-//            "likes": "23456"
-//        ],
-//        [
-//            "user": "Brandon",
-//            "imageURL": "http://s8.favim.com/610/150421/alternative-art-background-beautiful-Favim.com-2669488.jpg",
-//            "likes": "221"
-//        ],
-//        [
-//            "user": "Kyle",
-//            "imageURL": "http://s7.favim.com/610/151205/beach-boho-bright-instagram-Favim.com-3708977.jpg",
-//            "likes": "3626"
-//        ]
-//    ]
-    
     @IBOutlet var postListView : UITableView!
     
     let _uiImageID = 1
@@ -184,20 +106,6 @@ class FeedViewController : UIViewController, UITableViewDelegate, UITableViewDat
         return cell
         
     }
-    
-    // uses button postion within list view to figure out what row it is.
-//    func usernameButtonClicked(_ sender: UIButton){
-//        let position : CGPoint = sender.convert(CGPoint.zero, to: self.postListView)
-//        let indexPath = self.postListView.indexPathForRow(at: position)
-//        print(indexPath!.row)
-//        print(posts[indexPath!.row]["user"]!)
-//        print(posts[indexPath!.row]["imageURL"]!)
-//        
-//        let vc = self.storyboard?.instantiateViewController(withIdentifier: "profileView") as! ProfileViewController
-//        vc.setupFriendProfile(username: posts[indexPath!.row]["user"]!)
-//        self.navigationController?.pushViewController(vc, animated: true)
-//        
-//    }
     
 }
 
