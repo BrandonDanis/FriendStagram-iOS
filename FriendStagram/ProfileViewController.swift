603d70a625a4fdc13994c074d7bef6ff3ac9e43f
Brandon Danis <bdani093@uottawa.ca>
2017-03-27 20:59:43 -0400

    Removed un-used post array

--- FriendStagram/ProfileViewController.swift
+++ FriendStagram/ProfileViewController.swift
@@ -19,24 +19,6 @@ struct User {
 
 class ProfileViewController : UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    var images = [
-        "http://4.bp.blogspot.com/-F_6SfcFHKRE/UIjJKWfbt8I/AAAAAAAAA6w/AK5H_oGl9io/s1600/nature182.jpg",
-        "http://rising.blackstar.com/wp-content/uploads/2012/08/95432c1c89bd11e1a9f71231382044a1_7-450x450.jpg",
-        "https://s-media-cache-ak0.pinimg.com/originals/58/50/77/585077f705e1e1e6385940fee0e6a4d7.jpg",
-        "http://blog.ink361.com/wp-content/uploads/2014/11/@hamiltonguevara-.jpg",
-        "https://s-media-cache-ak0.pinimg.com/564x/94/a9/db/94a9db37f653d29075f14611c1bd7359.jpg",
-        "http://blogcdn.befunky.com/wp-content/uploads/2015/11/Screen-shot-2015-11-09-at-12.55.33-PM.png",
-        "http://imgs.abduzeedo.com/files/paul0v2/livefolk/livefolk-07.jpg",
-        "http://hahaha.in/wp-content/uploads/2013/04/8e744a5aa38511e2b8e822000a1fbcc7_72.jpg",
-        "http://s1.favim.com/610/150710/art-background-beautiful-dark-Favim.com-2933701.jpg",
-        "http://66.media.tumblr.com/tumblr_mdd8uyvpgQ1reash6o2_1280.jpg",
-        "http://stupiddope.com/wp-content/uploads/2014/07/cory-staudacher-instagram-03.jpg",
-        "http://static.boredpanda.com/blog/wp-content/uploads/2015/09/nature-photography-men-of-instagram-16__605.jpg",
-        "http://i.imgur.com/mpHgYA0.jpg",
-        "http://s14.favim.com/610/160212/adventure-fun-girl-nature-Favim.com-3985099.jpg",
-        "http://s8.favim.com/610/150421/alternative-art-background-beautiful-Favim.com-2669488.jpg",
-        "http://s7.favim.com/610/151205/beach-boho-bright-instagram-Favim.com-3708977.jpg"];
-    
     @IBOutlet var collectionView : UICollectionView!
     
     private var _username : String = "A Very Long Username"
@@ -75,10 +57,6 @@ class ProfileViewController : UIViewController, UICollectionViewDelegate, UIColl
         let logoutBarButton = UIBarButtonItem(customView: logoutButton)
         navigationItem.rightBarButtonItem = logoutBarButton
         navigationItem.rightBarButtonItem?.imageInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        print("Profile about to load")
     }
     
     func setupFriendProfile(username: String){
