ce56873803d6608cdf4882815e19b037f94e3e20
Brandon Danis <bdani093@uottawa.ca>
2017-04-18 00:37:14 -0400

    #3 Changed table view to custom static cells

--- FriendStagram/SettingsViewController.swift
+++ FriendStagram/SettingsViewController.swift
@@ -11,7 +11,31 @@ import UIKit
 
 class SettingsViewController : UITableViewController {
     
+    @IBOutlet var updateProfileCell : UITableViewCell!
+    @IBOutlet var updatePasswordCell : UITableViewCell!
+    
+    @IBOutlet var spacingCell : UITableViewCell!
+    
     override func viewDidLoad() {
+        
+        //Navigation bar
+        self.navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Billabong", size: 28)!,  NSForegroundColorAttributeName: Style.navigation_title_color]
+        self.navigationController?.navigationBar.tintColor = Style.navigation_title_color
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        self.navigationController?.navigationBar.barTintColor = Style.navigation_bar_color
+        
+        updateProfileCell.selectionStyle = .none
+        updateProfileCell.backgroundColor = UIColor.black
+        updateProfileCell.textLabel?.textColor = UIColor.white
+        
+        updatePasswordCell.selectionStyle = .none
+        updatePasswordCell.backgroundColor = UIColor.black
+        updatePasswordCell.textLabel?.textColor = UIColor.white
+        
+        spacingCell.selectionStyle = .none
+        spacingCell.backgroundColor = UIColor.black
+        
+        tableView.backgroundColor = UIColor.black
         
     }
     
