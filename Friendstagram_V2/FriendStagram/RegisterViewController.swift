f1aec45c0ef7d3effc925169c685e1f9e8517421
Brandon Danis <bdani093@uottawa.ca>
2017-11-08 18:08:06 -0500

    added registerViewController

---
+++ Friendstagram_V2/FriendStagram/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  FriendStagram
+//
+//  Created by Brandon Danis on 2017-11-08.
+//  Copyright © 2017 Brandon Danis. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+    }
+    
+}
