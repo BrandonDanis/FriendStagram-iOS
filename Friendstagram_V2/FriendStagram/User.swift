98801df385fa7001614ff36e529a7981654cc24f
brandondanis <brandon.danis@qlik.com>
2017-11-16 14:52:22 -0500

    Added user object

---
+++ Friendstagram_V2/FriendStagram/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  FriendStagram
+//
+//  Created by Brandon Danis on 11/16/17.
+//  Copyright © 2017 Brandon Danis. All rights reserved.
+//
+
+import Foundation
+
+struct User : Codable {
+    let name : String
+    let username : String
+    let id : Int
+    let datecreated : String
+}
