bf3ec66ae9866cbe997a8a8caaef4772beaa6f69
Brandon Danis <bdani093@uottawa.ca>
2017-11-25 11:43:00 -0500

    Playing around with feed cell

--- Friendstagram_V2/FriendStagram/Custom Elements/FeedViewCell.swift
+++ Friendstagram_V2/FriendStagram/Custom Elements/FeedViewCell.swift
@@ -13,10 +13,26 @@ class FeedViewCell : UITableViewCell {
     
     var label = UILabel()
     
+    let topView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = Colors.SOFT_PURPLE
+        return view
+    }()
+    
+    let postImage : UIImageView = {
+        let image = UIImageView(image: UIImage(named: "placeholder"))
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        contentView.addSubview(label)
+        contentView.addSubview(topView)
+        contentView.addSubview(postImage)
+        
+        topView.addSubview(label)
         
         label.text = "fasdasdfasdf"
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -25,15 +41,38 @@ class FeedViewCell : UITableViewCell {
         label.textColor = .black
         label.numberOfLines = 0
         
-        let margins = contentView.layoutMarginsGuide
-        label.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -20).isActive = true
-        label.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 20).isActive = true
-        label.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
-        label.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        ApplyConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func ApplyConstraints() {
+        
+        
+        //Top view
+        topView.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
+        topView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        
+        // Top view subviews
+        let topViewMargins = topView.layoutMarginsGuide
+        label.trailingAnchor.constraint(equalTo: topViewMargins.trailingAnchor, constant: -20).isActive = true
+        label.leadingAnchor.constraint(equalTo: topViewMargins.leadingAnchor, constant: 20).isActive = true
+        label.topAnchor.constraint(equalTo: topViewMargins.topAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: topViewMargins.bottomAnchor).isActive = true
+        
+        // image view subviews
+        postImage.topAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
+        postImage.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
+        postImage.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        
+        postImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        postImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        
+        postImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        
     }
     
 }
