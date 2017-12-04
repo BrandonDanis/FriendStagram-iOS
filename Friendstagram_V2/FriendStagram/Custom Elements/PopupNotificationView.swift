9d5bc86f46a0d75cdff7044fd1df2334d2fd032e
brandondanis <brandon.danis@qlik.com>
2017-12-04 12:30:32 -0500

    Created PopupNotificationView

---
+++ Friendstagram_V2/FriendStagram/Custom Elements/PopupNotificationView.swift
@@ -0,0 +1,16 @@
+//
+//  PopupNotificationView.swift
+//  FriendStagram
+//
+//  Created by Brandon Danis on 12/4/17.
+//  Copyright © 2017 Brandon Danis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PopupNotificationView : UIView {
+    
+    
+    
+}
