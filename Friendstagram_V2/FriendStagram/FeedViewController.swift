37348e82259009c98b5aff006ac06b0a5acecc77
Brandon Danis <bdani093@uottawa.ca>
2017-12-04 00:07:51 -0500

    Adding more fake posts for testing

--- Friendstagram_V2/FriendStagram/FeedViewController.swift
+++ Friendstagram_V2/FriendStagram/FeedViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class FeedViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     private var posts : [Post] = [
-        Post(id: 1, desc: "This is my desc", image: "https://i.redd.it/sbuv3lwq1h101.jpg", user_id: 1, username: "Brandon Danis"),
-        Post(id: 1, desc: "This is my desc", image: "https://i.redd.it/sbuv3lwq1h101.jpg", user_id: 1, username: "Brandon Danis"),
-        Post(id: 1, desc: "This is my desc", image: "https://i.redd.it/sbuv3lwq1h101.jpg", user_id: 1, username: "Brandon Danis"),
-        Post(id: 1, desc: "This is my desc", image: "https://i.redd.it/sbuv3lwq1h101.jpg", user_id: 1, username: "Brandon Danis"),
+        Post(id: 1, desc: "I personally love the new hexagonal style angel eyes", image: "https://i.redd.it/167u5u6r8ltz.jpg", user_id: 1, username: "BMW"),
+        Post(id: 1, desc: "This is my desc", image: "https://i.redd.it/uw97nvc6rjfz.jpg", user_id: 1, username: "BMW"),
+        Post(id: 1, desc: "This is my desc", image: "https://i.redd.it/sbuv3lwq1h101.jpg", user_id: 1, username: "BMW"),
+        Post(id: 1, desc: "Since you all liked the 1m drawing so much . I followed it up with an M2", image: "https://i.redd.it/vea3mvaxs8sz.jpg", user_id: 1, username: "BMW"),
         Post(id: 2, desc: "This is a lovely picture that I took myself with my camera which I then decided that all my friends on FriendStagram should see!", image: "https://i.imgur.com/WqVxsGS.jpg", user_id: 1, username: "Vivian Germain"),
-        Post(id: 3, desc: "This is a picture of a potato. What a great potato it is.", image: "https://i.imgur.com/WqVxsGS.jpg", user_id: 3, username: "Potato Lover")
+        Post(id: 3, desc: "This is a picture of a potato. What a great potato it is.", image: "https://i.imgur.com/WqVxsGS.jpg", user_id: 3, username: "Potato Lover"),
+        Post(id: 3, desc: "Absolutely obsessed with this car", image: "https://i.redd.it/evkcc9th1dmz.jpg", user_id: 3, username: "BMW"),
+        Post(id: 3, desc: "M4", image: "https://i.reddituploads.com/5a488ee92ef346389bfef38afa3fc3b0?fit=max&h=1536&w=1536&s=4848737a98e2818c2818a2458d112bc0", user_id: 3, username: "BMW")
     ]
     
     private var tableView : UITableView = {
