d1c10f9e6a2178774a8131f0282c0a0925f0d855
Brandon Danis <bdani093@uottawa.ca>
2018-01-06 11:07:00 -0500

    Small changes on static posts

--- Friendstagram_V2/FriendStagram/Models/Post.swift
+++ Friendstagram_V2/FriendStagram/Models/Post.swift
@@ -27,12 +27,12 @@ struct Post : Codable {
 
 let TEST_POSTS : [Post] = [
     Post(id: 1, desc: "I personally love the new hexagonal style angel eyes 😍", image: "https://i.redd.it/167u5u6r8ltz.jpg", user_id: 1, username: "BMW"),
-    Post(id: 1, desc: "This is my desc", image: "https://i.redd.it/uw97nvc6rjfz.jpg", user_id: 1, username: "BMW"),
-    Post(id: 1, desc: "This is my desc", image: "https://i.redd.it/sbuv3lwq1h101.jpg", user_id: 1, username: "BMW"),
-    Post(id: 1, desc: "Since you all liked the 1m drawing so much . I followed it up with an M2", image: "https://i.redd.it/vea3mvaxs8sz.jpg", user_id: 1, username: "BMW"),
-    Post(id: 2, desc: "This is a lovely picture that I took myself with my camera which I then decided that all my friends on FriendStagram should see!", image: "https://i.imgur.com/tf9GpKr.jpg", user_id: 1, username: "Vivian Germain"),
-    Post(id: 3, desc: "Quite happy with how this shot of my friends M4 came out", image: "https://i.redd.it/u7tj49w3uu101.jpg", user_id: 3, username: "BMW Lover"),
-    Post(id: 3, desc: "Absolutely obsessed with this car", image: "https://i.redd.it/evkcc9th1dmz.jpg", user_id: 3, username: "BMW"),
-    Post(id: 3, desc: "M4 👌🏾", image: "https://i.reddituploads.com/5a488ee92ef346389bfef38afa3fc3b0?fit=max&h=1536&w=1536&s=4848737a98e2818c2818a2458d112bc0", user_id: 3, username: "BMW"),
-    Post(id: 4, desc: "Rainy days 😍", image: "https://i.redd.it/yyfwtc18fn201.jpg", user_id: 8, username: "BMW_LOVER")
+    Post(id: 2, desc: "This is my desc", image: "https://i.redd.it/uw97nvc6rjfz.jpg", user_id: 1, username: "BMW"),
+    Post(id: 3, desc: "This is my desc", image: "https://i.redd.it/sbuv3lwq1h101.jpg", user_id: 1, username: "BMW"),
+    Post(id: 4, desc: "Since you all liked the 1m drawing so much . I followed it up with an M2", image: "https://i.redd.it/vea3mvaxs8sz.jpg", user_id: 1, username: "BMW"),
+    Post(id: 5, desc: "This is a lovely picture that I took myself with my camera which I then decided that all my friends on FriendStagram should see!", image: "https://i.imgur.com/tf9GpKr.jpg", user_id: 2, username: "Vivian Germain"),
+    Post(id: 6, desc: "Quite happy with how this shot of my friends M4 came out", image: "https://i.redd.it/u7tj49w3uu101.jpg", user_id: 1, username: "BMW Lover"),
+    Post(id: 7, desc: "Absolutely obsessed with this car", image: "https://i.redd.it/evkcc9th1dmz.jpg", user_id: 1, username: "BMW"),
+    Post(id: 8, desc: "M4 👌🏾", image: "https://i.reddituploads.com/5a488ee92ef346389bfef38afa3fc3b0?fit=max&h=1536&w=1536&s=4848737a98e2818c2818a2458d112bc0", user_id: 1, username: "BMW"),
+    Post(id: 9, desc: "Rainy days 😍", image: "https://i.redd.it/yyfwtc18fn201.jpg", user_id: 1, username: "BMW_LOVER")
 ]
